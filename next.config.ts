9b8560d41a66d00c01e49e7af5a3e6c273a50b11
Nick Vigier <nvigier46@gmail.com>
2025-03-24 16:29:27 -0700

    Add redirects to next.config.ts for external URL handling

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,21 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  redirects: async () => {
+    return [
+      {
+        source: '/:path*',
+        destination: 'https://00415268-middleware-params.preview.vercel-support.app/:path*', // new domain
+        statusCode: 307,
+        missing: [
+          {
+            'value': '00415268-middleware-params.preview.vercel-support.app', // old domain
+            'type': 'host'
+          }
+        ]
+      }
+    ]
+  }
 };
 
 export default nextConfig;
